411b579cf6a97b3baf01e8847453114bef527bc3
LanTran2006 <lantrancute2006@gmail.com>
2025-04-27 20:28:04 +0700

    add contact page

--- client/src/pages/contact/Contact.jsx
+++ client/src/pages/contact/Contact.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import Header from "../../components/Header/Header";
 import Section from "./Section";
 import Footer from '../home/Footer'
-
+import Loader from '../../components/loading'
 const Contact = () => {
   const [sections, setSections] = useState([])
   const [loading, setLoading] = useState(false)
@@ -10,7 +10,7 @@ const Contact = () => {
     const fetchData = async () => {
       setLoading(true)
       try {
-        const response = await fetch('http://localhost:5000/api/v1/section/')
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/section`)
         const result = await response.json()
         if (Array.isArray(result)) {
           setSections(result);
@@ -36,14 +36,14 @@ const Contact = () => {
     return (
       <div className="bg-cover bg-center bg-[url('/bg/bgHome.jpg')] text-white">
         <Header/>
-        <div className="text-white text-center py-20">Đang tải dữ liệu...</div>
+        <Loader/>
       </div>
   )
   }
   return (
     <div className="bg-cover bg-center bg-[url('/bg/bgHome.jpg')]">
       <Header />
-      <section className="text-center py-16 text-white">
+      <section className="text-center py-16 text-white min-h-[90vh]">
         <h1 className="text-4xl font-bold mb-4">KHƠI NGUỒN TRI THỨC</h1>
         <p className="text-lg">
           Nơi cung cấp cho bạn những thông tin bổ ích về văn hóa, lịch sử và
